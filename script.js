a7aa767a13b3a694066eab13685786fd61358c4f
Erick Rivera <erickriverart@gmail.com>
2026-05-29 22:38:39 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+const targetDate = new Date("June 27, 2026 00:00:00").getTime();
+
+function updateCountdown(){
+
+const now = new Date().getTime();
+
+const distance = targetDate - now;
+
+const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+const hours = Math.floor(
+(distance % (1000 * 60 * 60 * 24))
+/
+(1000 * 60 * 60)
+);
+
+const minutes = Math.floor(
+(distance % (1000 * 60 * 60))
+/
+(1000 * 60)
+);
+
+const seconds = Math.floor(
+(distance % (1000 * 60))
+/
+1000
+);
+
+document.getElementById("days").innerHTML = days;
+document.getElementById("hours").innerHTML = hours;
+document.getElementById("minutes").innerHTML = minutes;
+document.getElementById("seconds").innerHTML = seconds;
+
+}
+
+updateCountdown();
+
+setInterval(updateCountdown,1000);
